c6bcd1168c3e234365ef8166ffa10589ed96dee1
Thivstan James <114639488+thivistan@users.noreply.github.com>
2024-02-28 21:33:41 -0800

    Update experiences.js

--- src/data/experiences.js
+++ src/data/experiences.js
@@ -11,18 +11,19 @@ export const experiences = [
     joined: "Sep 2023",
     end: "Dec 2023",
     title: "Software Developer",
-    bio:"In my role as a Software Developer from Sep 2023 to Dec 2023,  proficient in Angular, TypeScript, and HTML/CSS. Led teams to improve UI responsiveness by 70% and drive a 25% increase in user satisfaction through feature development. Implemented advanced rendering techniques, reducing rendering time by 40% and improving project delivery speed by 25%. Automated 60% of infrastructure tasks, boosting code quality by 45%. Spearheaded GraphQL adoption, resulting in a 30% API efficiency surge. Conducted bi-weekly code reviews, achieving 95% code coverage and reducing critical issues by 30%. Passionate about delivering innovative solutions that optimize performance and elevate user experience.",
-    color: blue,
+    bio: "As a Software Developer, I led teams to enhance UI responsiveness by 70% and drive a 25% increase in user satisfaction through feature development. I implemented advanced rendering techniques, reducing rendering time by 40% and improving project delivery speed by 25%. Automating 60% of infrastructure tasks boosted code quality by 45%. Spearheading GraphQL adoption resulted in a 30% API efficiency surge. I conducted bi-weekly code reviews achieving 95% code coverage, reducing critical issues by 30%.",
+    color: "blue",
   },
+
   {
+    {
     logo: accenture,
     name: "Accenture",
     joined: "Jan 2021",
     end: "Aug 2022",
     title: "Software Engineer",
-    bio: "As a Software Engineer from January 2021 to August 2022,  Focused on frontend and backend development, specializing in JavaScript, ReactJS, and Spring Boot. Notable achievements include enhancing user retention by 40% through frontend enhancements and improving system performance by 30% via advanced Redux state management techniques. Proficient in Java microservices development, achieving exceptional fault tolerance rates of 99.8%. Demonstrated ability to resolve urgent production issues swiftly, ensuring 100% client satisfaction. Leveraged Spring Cloud to implement secure authentication and enhance system responsiveness by 30%. Led Agile teams to deliver over 10 applications on AWS, utilizing Docker, Kubernetes, and Jenkins for 75% faster deployments and a 35% productivity gain.",
-    //bio: "I excelled in problem-solving, achieving 100% customer satisfaction by swiftly resolving production issues using SQL and Stored Procedures. Leading an Agile team, I orchestrated the delivery of numerous applications on AWS, accelerating deployment and enhancing productivity by 35%. Implementing Spring Cloud and optimizing algorithms, I significantly improved system responsiveness while reducing memory usage by 65%.",
-    color: green,
+    bio: "As a versatile Software Engineer proficient in JavaScript, ReactJS, Spring Boot, and Java microservices, I've achieved notable successes in frontend and backend development. I enhanced user retention by 40% through frontend optimizations and improved system performance by 30% with advanced Redux state management techniques. My expertise extends to AWS, Docker, Kubernetes, and Jenkins, facilitating 75% faster deployments and a 35% productivity gain. Additionally, I orchestrated the seamless migration to NetSuite ERP, reducing costs by $50,000 annually while increasing efficiency by 40%.",
+    color: "green",
   },
   
 ];
