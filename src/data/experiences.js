f8e38c432bcfb44e2b23ec5dac4b42c54a5add93
Thivstan James <114639488+thivistan@users.noreply.github.com>
2024-02-28 22:03:45 -0800

    Update experiences.js

--- src/data/experiences.js
+++ src/data/experiences.js
@@ -12,7 +12,7 @@ export const experiences = [
     end: "Dec 2023",
     title: "Software Developer",
     bio: "As a Software Developer, I led teams to enhance UI responsiveness by 70% and drive a 25% increase in user satisfaction through feature development. I implemented advanced rendering techniques, reducing rendering time by 40% and improving project delivery speed by 25%. Automating 60% of infrastructure tasks boosted code quality by 45%. Spearheading GraphQL adoption resulted in a 30% API efficiency surge. I conducted bi-weekly code reviews achieving 95% code coverage, reducing critical issues by 30%.",
-    color: "blue",
+    color: blue,
   },
 
     {
@@ -22,7 +22,7 @@ export const experiences = [
     end: "Aug 2022",
     title: "Software Engineer",
     bio: "As a versatile Software Engineer proficient in JavaScript, ReactJS, Spring Boot, and Java microservices, I've achieved notable successes in frontend and backend development. I enhanced user retention by 40% through frontend optimizations and improved system performance by 30% with advanced Redux state management techniques. My expertise extends to AWS, Docker, Kubernetes, and Jenkins, facilitating 75% faster deployments and a 35% productivity gain. Additionally, I orchestrated the seamless migration to NetSuite ERP, reducing costs by $50,000 annually while increasing efficiency by 40%.",
-    color: "green",
+    color: green,
   },
   
 ];
